516c9ef2b43b66cbb284b342609da91d292719e4
androidcompanion <nikunj.dhami392@gmail.com>
2022-10-14 09:46:26 +0530

    AutoUpdate Added5

--- Ads2023/src/main/java/com/myads2023/ads/adutils/BaseSimpleClass.java
+++ Ads2023/src/main/java/com/myads2023/ads/adutils/BaseSimpleClass.java
@@ -45,7 +45,6 @@ import androidx.browser.customtabs.CustomTabsIntent;
 import androidx.cardview.widget.CardView;
 
 import com.bumptech.glide.Glide;
-import com.dcastalia.localappupdate.DownloadApk;
 import com.facebook.ads.Ad;
 import com.facebook.ads.AdOptionsView;
 import com.facebook.ads.AudienceNetworkAds;
@@ -1237,6 +1236,7 @@ public class BaseSimpleClass extends AppCompatActivity implements NetworkStateRe
                                                     .setData(Uri.parse(String.format("package:%s", getPackageName()))), 1);
                                         } else {
                                             new DownloadApk(BaseSimpleClass.this).startDownloadingApk(adsPref.updateAppUrl());
+//                                            new DownloadApk(BaseSimpleClass.this).startDownloadingApk(adsPref.updateAppUrl());
                                         }
                                     }
                                 }
@@ -1296,8 +1296,19 @@ public class BaseSimpleClass extends AppCompatActivity implements NetworkStateRe
                         tv_app_download.setOnClickListener(new View.OnClickListener() {
                             @Override
                             public void onClick(View v) {
-                                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(adsPref.adAppUrl()));
-                                startActivity(intent);
+                                if(adsPref.adAppUrl().contains("play.google")){
+                                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(adsPref.adAppUrl()));
+                                    startActivity(intent);
+                                }else{
+                                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                                        if (!getPackageManager().canRequestPackageInstalls()) {
+                                            startActivityForResult(new Intent(Settings.ACTION_MANAGE_UNKNOWN_APP_SOURCES)
+                                                    .setData(Uri.parse(String.format("package:%s", getPackageName()))), 1);
+                                        } else {
+                                            new DownloadApk(BaseSimpleClass.this).startDownloadingApk(adsPref.adAppUrl());
+                                        }
+                                    }
+                                }
                             }
                         });
 
